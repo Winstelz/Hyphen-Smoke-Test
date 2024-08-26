3d112afdcd2b80f06218064736b95fc9b211c91d
Winstelz <Winstelz@gmail.com>
2024-08-26 09:58:19 -0400

    Add files via upload

--- AmazonCarousel.spec.ts
+++ AmazonCarousel.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 
 
-test('WalMart Carousel', async ({ page }) => {
+test('Amazon Carousel', async ({ page }) => {
     //Navigate to Advantage site    
         await page.goto('https://www.hyphen.shopping/0QRNY52VKA');
 //Change Store
@@ -17,28 +17,28 @@ test('WalMart Carousel', async ({ page }) => {
     await Zip.click();
     await Zip.fill("45014");
     await page.keyboard.press("Enter");
-    const WalMart = page.locator("//li[8]//section[2]//button[1]//div[1]//span[1]");
-    await WalMart.click();
+    const Amazon = page.locator("(//span[@class='css-1d8cyf9'][normalize-space()='SELECT'])[1]");
+    await Amazon.click();
     await page.waitForTimeout(2000);
-    const Address = page.locator("//span[@class='css-ue1hsf']");
-    expect(Address).toContainText("1143");
+    const Address = page.locator("//span[@class='css-itlzwm']");
+    expect(Address).toContainText("Amazon");
 //Click First Product
     const FirstProd = await page.locator("//body/div[@id='root']/div[@class='css-1dt9zfm']/section[@class='css-qo9iz4']/div[@class='css-0']/div[@class='css-1ug2bif']/div[@class='css-tomjeu']/article[1]/div[1]/div[1]");
     await FirstProd.click();
     await page.waitForTimeout(2000);
-    const RightArrow = page.locator("//button[@class='css-2a1iib']");
+    const RightArrow = page.locator("//button[@class='css-2a1iib']//*[name()='svg']");
     await RightArrow.click();
     await RightArrow.click();
     await RightArrow.click();
     await RightArrow.click();
     await page.waitForTimeout(2000);
-    const LastImg = page.locator("//li[@class='css-98o3qm']//img[@alt='Product thumbnail']");
-    expect(LastImg).toHaveAttribute("src", "https://i5.walmartimages.com/asr/d84aff5e-faa1-4013-955b-846adcdced5b.b43920dabf21351948caa168ddc78c99.jpeg?odnHeight=100&odnWidth=100&odnBg=ffffff");
+    const LastImg = page.locator("(//img[@alt='Product thumbnail'])[5]");
+    expect(LastImg).toHaveAttribute("src", "https://images-na.ssl-images-amazon.com/images/I/81ZBH43QdAL.jpg");
     const LeftArrow = page.locator("//button[@class='css-12ztixw']//*[name()='svg']");
     await LeftArrow.click();
     await LeftArrow.click();
     const ThirdImg = page.locator("//li[@class='css-98o3qm']//img[@alt='Product thumbnail']");
-    expect(ThirdImg).toHaveAttribute("src", "https://i5.walmartimages.com/asr/2603ebff-ffd6-4c7f-bb43-41271b8d19ad.50867bfce8132e93312c3d8bb777bb26.jpeg?odnHeight=100&odnWidth=100&odnBg=ffffff");
+    expect(ThirdImg).toHaveAttribute("src", "https://images-na.ssl-images-amazon.com/images/I/81ZBH43QdAL.jpg");
     const Close = page.locator("//button[@class='css-t2aoxx']//*[name()='svg']");
     await Close.click();
 //Click Last Product
@@ -48,28 +48,26 @@ test('WalMart Carousel', async ({ page }) => {
     await RightArrow.click();   
     await RightArrow.click();
     await RightArrow.click();
-    await RightArrow.click();
     await page.waitForTimeout(2000);
-    const LastImg2 = page.locator("//div[@id='gallerySlider']//li[5]//img[1]");
-    expect(LastImg2).toHaveAttribute("src", "https://i5.walmartimages.com/asr/7c654e03-a479-491a-92e1-3fd66407ee16.904076ff2fedae0dec9fce51188d626d.jpeg?odnHeight=100&odnWidth=100&odnBg=ffffff");
-    await LeftArrow.click();
-    await LeftArrow.click();    
-    expect(ThirdImg).toHaveAttribute("src", "https://i5.walmartimages.com/asr/bc921b15-dfa8-48bd-a9da-e710935bf00f.06fcac51bb927b89d6fb5bb37db114e5.jpeg?odnHeight=100&odnWidth=100&odnBg=ffffff");
+    const LastImg2 = page.locator("//div[@id='gallerySlider']//li[4]//img[1]");
+    expect(LastImg2).toHaveAttribute("src", "https://images-na.ssl-images-amazon.com/images/I/61mIRGqG--L.jpg");
+    await LeftArrow.click();   
+    expect(ThirdImg).toHaveAttribute("src", "https://images-na.ssl-images-amazon.com/images/I/51HwKvZq0pL.jpg");
     await Close.click();
-    await page.waitForTimeout(2000);
+    //await page.waitForTimeout(2000);
 //Add to Cart Button
-    const FirstATC = await page.locator("(//button[@id='single-shop-now-button'])[2]").click();
+    const FirstATC = await page.locator("((//button[@aria-label='ATC'])[2]").click();
     const NewPage = await page.waitForEvent("popup");
     await NewPage.waitForTimeout(2000);
-    await expect(NewPage).toHaveTitle(/Gerber 2nd/);
+    await expect(NewPage).toHaveTitle(/Please Confirm/);
 //Close NewPage
     await NewPage.close();
 //Add to Cart From Product Title
     await LastProd.click();
-    const SecondATC = await page.locator("(//button[@id='single-shop-now-button'])[1]").click();
+    const SecondATC = await page.locator("(//button[@aria-label='ATC'])[6]").click();
     const NewPage2 = await page.waitForEvent("popup");
     await NewPage2.waitForTimeout(2000);
-    await expect(NewPage2).toHaveTitle(/Dove Refreshing/);
+    await expect(NewPage2).toHaveTitle(/Please Confirm/);
     
 
 
